84b2a7ef7fb21ad2823f4781991285d2b6974039
Maniteja_Gaddam <happyfamily110705@gmail.com>
2023-12-05 00:13:38 +0530

    Update BinaryTrees(Traversing).c
    added user input main and made the code optimized than the before

--- BinaryTrees(Traversing).c
+++ BinaryTrees(Traversing).c
@@ -8,74 +8,12 @@ struct Node{
 };
 
 
-struct Node *temp;
+struct Node *create_a_node( int val);
+struct Node *Insert_value(struct Node *root , int val);
+void InorderTraverse(struct Node * root);
+void  PostorderTraverse( struct Node * root);
+void PreorderTraverse(struct Node * root);
 
-struct Node *root = NULL;
-
-
-struct Node *create_a_node( int val){
-    
-    temp = (struct Node *) malloc (sizeof(struct Node));
-    temp -> data = val;
-    temp -> left = NULL;
-    temp -> right = NULL;
-    
-    return temp;
-}
-
-
-struct Node *Insert(struct Node *root , int val){
-    if (root == NULL){
-        return create_a_node(val);
-    }
-    if (val < root -> data){
-        root -> left = Insert(root -> left , val);
-    }else if( val > root -> data){
-        root -> right = Insert(root -> right , val);
-    }
-    return root;
-
-
-}
-/*InorderTraversal = 
-travesing direction:  left -> root -> right*/  
-
-struct Node *InorderTraverse(struct Node * root){
-
-    if(root != NULL){
-        InorderTraverse(root -> left);
-        printf("%d  ",root -> data);
-        InorderTraverse(root -> right);
-    }
-    
-    return NULL;
-}
-
-/*PostorderTraversal = 
-travesing direction:  left -> right -> root*/  
-
-struct Node * PostorderTraverse( struct Node * root){
-    if (root != NULL){
-        PostorderTraverse(root -> left);
-        PostorderTraverse(root -> right);
-        printf("%d  " , root -> data);
-    }
-
-    return NULL;
-}
-
-/*PreorderTraversal = 
-travesing direction: root -> left -> right*/  
-
-struct Node *PreorderTraverse(struct Node * root){
-    if(root != NULL){
-        printf("%d  ", root -> data);
-        PreorderTraverse(root -> left);
-        PreorderTraverse(root -> right);
-    }
-
-    return NULL;
-}
 
 
 /*
@@ -87,19 +25,146 @@ struct Node *PreorderTraverse(struct Node * root){
            20  40 60   80
 */
 
+// int main(){
+//     struct Node *root = NULL;
+//     root = Insert_value(root, 50);
+//     Insert_value(root, 30);
+//     Insert_value(root, 20);
+//     Insert_value(root, 40);
+//     Insert_value(root, 70);
+//     Insert_value(root, 60);
+//     Insert_value(root, 80);
+
+//     InorderTraverse(root);
+//     printf("\n");
+//     PostorderTraverse(root);
+//     printf("\n");
+//     PreorderTraverse(root);
+// }
+
 
 int main(){
-    root = Insert(root, 50);
-    Insert(root, 30);
-    Insert(root, 20);
-    Insert(root, 40);
-    Insert(root, 70);
-    Insert(root, 60);
-    Insert(root, 80);
+    struct Node * root = NULL;
+    int option;
 
-    InorderTraverse(root);
-    printf("\n");
-    PostorderTraverse(root);
-    printf("\n");
-    PreorderTraverse(root);
+    do {
+        printf("*****-----Main Menu-----*****\n");
+        printf("1.Insert a value\n");
+        printf("2.Delete a value\n");
+        printf("3.In order Traversing\n");
+        printf("4.Pre order Traversing\n");
+        printf("5.Post order Traversing\n");
+        printf("6.Height of Tree\n");
+        printf("*****------The End------*****\n");
+
+        printf("Choose any Option : ");
+        scanf("%d" ,&option);
+        int val;
+
+        switch (option)
+        {
+        case 1:
+            printf("Enter the value : ");
+            scanf("%d",&val);
+            root = Insert_value(root , val);
+            break;
+        
+        case 2:
+
+            break;
+
+        case 3:
+            InorderTraverse(root);
+            printf("\n");
+            break;
+
+        case 4:
+            PreorderTraverse(root);
+            printf("\n");
+            break;
+
+        case 5:
+            PostorderTraverse(root);
+            printf("\n");
+            break;
+
+        case 6:
+
+            break;
+        
+        case 7:
+            break;
+
+        default:
+            printf("Choose a valid option");
+        }
+    }while(option != 7);
 }
+
+struct Node *create_a_node( int val){
+    
+    struct Node *temp;
+    temp = (struct Node *) malloc (sizeof(struct Node));
+    temp -> data = val;
+    temp -> left = NULL;
+    temp -> right = NULL;
+    
+    return temp;
+}
+
+
+struct Node *Insert_value(struct Node *root , int val){
+
+    if (root == NULL){
+        return create_a_node(val);
+    }
+    if (val < root -> data){
+        root -> left = Insert_value(root -> left , val);
+    }else if( val > root -> data){
+        root -> right = Insert_value(root -> right , val);
+    }
+    return root;
+
+
+}
+
+
+/*InorderTraversal = 
+travesing direction:  left -> root -> right*/  
+
+
+void InorderTraverse(struct Node * root){
+
+    if(root != NULL){
+        InorderTraverse(root -> left);
+        printf("%d  ",root -> data);
+        InorderTraverse(root -> right);
+    }
+}
+
+
+/*PostorderTraversal = 
+travesing direction:  left -> right -> root*/  
+
+
+void PostorderTraverse( struct Node * root){
+    if (root != NULL){
+        PostorderTraverse(root -> left);
+        PostorderTraverse(root -> right);
+        printf("%d  " , root -> data);
+    }
+}
+
+
+/*PreorderTraversal = 
+travesing direction: root -> left -> right*/  
+
+
+void PreorderTraverse(struct Node * root){
+    if(root != NULL){
+        printf("%d  ", root -> data);
+        PreorderTraverse(root -> left);
+        PreorderTraverse(root -> right);
+    }
+}
+
